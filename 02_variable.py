7d72794d06f0dc467d0910fbfdda5ac75770a255
Ayush kumar <kumarayush7942@gmail.com>
2021-07-27 11:42:10 +0530

    update 02_variable.py with types of variables

--- 02_variable.py
+++ 02_variable.py
@@ -5,6 +5,16 @@ good
 boy'''
 b=23
 c=23.45
+d=True
+
+#print the values of variables
 print(a)
 print(b)
 print(c)
+print(d)
+
+#print the types of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
